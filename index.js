34f6a0f1a70316b8a4541c939d7e8acade28417b
Lunga Booi <Lungalucasbooi@gmail.com>
2022-08-16 13:23:22 +0200

    Updates

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const cors = require("cors"); // Used to prevent errors when working locally
 const path = require("path")
 
 const app = express(); // Initialize express as an app variable
-app.set("port", process.env.PORT || 9000); // Set the port
+app.set("port", process.env.PORT || 3000); // Set the port
 app.use(express.json()); // Enable the server to handle JSON requests
-app.use(cors()); // Dont let local development give errors
+app.use(cors({
+    origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
+    credentials: true,
+
+})); // Dont let local development give errors
 
 
 const staticPath = path.join(__dirname + 'public')
@@ -15,19 +19,27 @@ app.use(express.static(staticPath))
 // Import routes
 const userRoute = require("./routes/userRoute");
 const productsRoute = require("./routes/productsRoute");
+const cartRoute = require("../Backend/routes/cartRoute");
 
 
-
+// Login page
 app.use(express.static("public"));//Static
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/" + "index.html")
 });
 
+app.use(express.static("public"));//Static
+
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/" + "register.html")
+});
+
 app.use("/users", userRoute);
 
 app.use("/products", productsRoute); 
 
+app.use("/cart", cartRoute);
 
 
 
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const con = require("../lib/db_connection");
 
-router.get("/", (req, res) => {
+//Get cart by Id from the database
+router.get("/:id", (req, res) => {
     try {
-        con.query(`SELECT * FROM categories`, (err, result) => {
+        con.query(`SELECT * FROM cart WHERE cart_id =${req.params.id}`, (err, result) => {
             if (err) throw err;
             res.send(result);
         });
@@ -12,7 +13,19 @@ router.get("/", (req, res) => {
         console.log(error);
         res.status(400).send(error)
     }
-});
+  });
+
+  router.delete("/:id", (req, res) => {
+    try {
+        con.query(`DELETE  FROM cart WHERE cart_id =${req.params.id}`, (err, result) => {
+            if (err) throw err;
+            res.send(result);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error)
+    }
+  });
  
 router.post('/', (req, res)=> {
     const { name, description, thumbnail,}= req.body
